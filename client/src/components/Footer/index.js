a95deca06906d85fc7de35e96eabff6eb3c4b68a
melissa-tan <melissatan9683@gmail.com>
2021-07-12 21:45:53 -0700

    minor updates on other components

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from "react"; 
+import './style.css';
+
+
+    export default function Footer() {
+        return (
+            <React.Fragment>
+                <div className='footer'>If you have any questions, comments or issues, please submit an <a href="https://github.com/JosephCordell/ListablyReact/issues">issue</a> on Github</div>
+            </React.Fragment>
+        )
+    }
