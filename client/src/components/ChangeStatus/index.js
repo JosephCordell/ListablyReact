5b0b43d6f3e93748f8604f3c2dcfa4e46936f15a
Joseph Cordell <joseph@jcordell.com>
2021-07-14 14:34:45 -0700

    Updating Change Status

--- client/src/components/ChangeStatus/index.js
+++ client/src/components/ChangeStatus/index.js
@@ -1,10 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-export default function ChangeStatus() {
+export default function ChangeStatus({ movie }) {
+    const movieDetails = movie
+    const [value, setValue ] = useState()
+
+    useEffect(() => {
+
+        const missive = {
+            title: movieDetails.title,
+            release_date: movieDetails.date,
+            poster_path: movieDetails.poster,
+            mediatype: movieDetails.type,
+            id: movieDetails.id,
+            todo: value,
+            description: movieDetails.overview,
+        }; 
+/* 
+        fetch('/api/media', {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(missive),
+        })
+            .then((response) => {
+                if (response.status === 401) {
+                    document.location.replace('/login');
+                }
+            })
+            .catch((error) => console.log(error));
+     */
+
+
+
+    }, [value])
+
+
     return (
         <React.Fragment>
-            <select className={"changeStatus"}>
-                <option value="default" disabled hidden>
+            <select className={"changeStatus"} onChange={(e) => setValue(e.target.value)}>
+                <option defaultValue="default"  disabled hidden>
                     Add to my list:
                 </option>
                 <option value="0">Watching</option>
