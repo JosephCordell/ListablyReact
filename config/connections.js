5389bef98e29a59df5260e8eb4a49e333d786a8b
melissa-tan <melissatan9683@gmail.com>
2021-07-17 11:28:04 -0700

    minor css updates

--- config/connections.js
+++ config/connections.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 
 let sequelize;
 
-if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
+if (process.env.REACT_APP_JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.REACT_APP_JAWSDB_URL);
 } else {
     sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
         host: 'localhost',
