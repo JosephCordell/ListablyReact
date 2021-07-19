acff1a82241072deabcc507ce5e2834163bb79b6
Joseph Cordell <joseph@jcordell.com>
2021-07-19 09:01:02 -0700

    Changed to export defualt

--- client/src/js/API.js
+++ client/src/js/API.js
@@ -1,4 +1,5 @@
-module.exports = {
+/* eslint-disable import/no-anonymous-default-export */
+export default {
     media: {
         get: async () => {
             return await fetch('/api/media/get', {
