722c24ca99e7d3a00d7f0b0fc0f387127ed4092d
pablo <paulandrewjeffrey@gmail.com>
2021-07-18 18:32:21 -0700

    add SIMILAR to USERLIST

--- client/src/components/UserList/index.js
+++ client/src/components/UserList/index.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
+import Similar from '../Similar';
+import axios from 'axios';
 
 export default function UserList({ medias }) {
+
+    const [ similar, setSimilar ] = useState([]);
+
+    const getSimilar = (event) => {
+        const ID = event.target.dataset.id;
+        const KEY = process.env.REACT_APP_MOVIE_DB_API_KEY;
+        const URL = `https://api.themoviedb.org/3/movie/${ ID }/similar?api_key=${ KEY }&language=en-US&page=1`;
+
+        axios.get(URL)
+            .then(response => setSimilar(response.data.results))
+            .catch(error => console.log(error));
+    };
+
+    const reset = (event) => {
+        setSimilar([]);
+    };
+
     return (
         <React.Fragment>
             {medias.length > 0 ? (
@@ -17,8 +36,15 @@ export default function UserList({ medias }) {
                                 data-description="{{media.overview}}"
                                 data-todo="{{media.todo}}"
                             >
+
                                 <img src={`https://image.tmdb.org/t/p/w500${media.poster_path}`} alt={media.mediatype} className="card-image" />
+                                
                                 <div className="card-title">{media.title}</div>
+
+                                {similar.length
+                                    ? <button onClick={ reset }>close</button>
+                                    : <button data-id={ media.id } onClick={ getSimilar }>More Like This</button>
+                                }
 
                                 <div className="dropDownUser">
                                     <select className="changeStatusUser" value={media.todo}>
@@ -28,6 +54,15 @@ export default function UserList({ medias }) {
                                         <option value="6">Dropped</option>
                                     </select>
                                 </div>
+
+                                <div className={'similar-container'}>
+                                    { similar.length > 0 
+                                        ? similar.map((similarThing) => (
+                                            <Similar similarThing={similarThing} key={similarThing.id} />)) 
+                                        : '' 
+                                    }
+
+                    </div>
                             </div>
                         </div>
                     ))}
