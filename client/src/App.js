cedbf3632d3ca5a0c57078648d9c4228f1df6127
Joseph Cordell <joseph@jcordell.com>
2021-07-20 08:50:58 -0700

    Removed comment

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Homepage from './views/Homepage';
-import { Route, Switch /* , Redirect */ } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import SignUp from './views/SignUp';
 import SearchResults from './views/SearchResults';
 import TrendingMovies from './views/TrendingMovies';
